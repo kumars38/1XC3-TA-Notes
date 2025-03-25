48ab6b0ef89fd490ecb7a9d89967677cd9a7a5ec
Samarth Kumar <skuminater@gmail.com>
2025-03-24 23:24:27 -0400

    add Q3 L05 solution

---
+++ quiz/Q3_tribonacci_rec.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+// assume 64-bit ans (ans <= 2^63 - 1)
+// we need a type with 8 bytes of data
+// int             - at least 2 bytes, usually 4 bytes (32-bit) on modern CPU
+// long (int)      - at least 4 bytes, usually 8 bytes (64-bit) on Linux/macOS
+// long long (int) - at least 8 bytes, "safe" 64-bit across OS, sometimes 128-bit
+
+// on most systems, long is sufficient
+// but to be safe across different OS, we can use long long
+// or use fixed-width, uint64_t from <stdint.h>
+unsigned long long tribonacci(const int n) {
+    // base case
+    if (n <= 0)
+        return 0;
+    if (n == 1 || n == 2)
+        return 1;
+    // recursive step
+    return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+}
+
+int main() {
+    int n = 5;
+    long long out = tribonacci(n);
+    printf("T_%d = %llu\n", n, out);
+}
